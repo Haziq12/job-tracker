50a562d00b8f5c4c18310d627e8b16b382d0cc9c
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-13 19:05:44 -0400

    landing page image appear on big screen only

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -38,6 +38,27 @@ nav{
   align-items: center;
   margin-top: -3rem;
 }
+h1{
+  font-weight: 700;
+  span{
+    color: var(--primary-500);
+  }
+}
+p{
+  color: var(--grey-600);
+}
+.main-img{
+  display: none;
+}
+@media (min-width: 992px) {
+  .page{
+    grid-template-columns: 1fr 1fr;
+    column-gap: 3rem;
+  }
+  .main-img{
+    display: block;
+  }
+}
 `
 
 export default Landing
