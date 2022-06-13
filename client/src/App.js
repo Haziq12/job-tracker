fd74c7d7ba1345a572df5c301bab4f775bae9618
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-13 18:49:28 -0400

    use styled css components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import Landing from "./pages/Landing";
+import styled from 'styled-components'
+
+const Button = styled.button`
+background: black;
+color: white;
+font-size: 1rem;
+`
 
 function App() {
   return (
     <div>
+      <Button>Click Me</Button>
       <h1>Job Tracker</h1>
       <Landing />
     </div>
