fd0a681682f72304086efd5acc28548da489a30c
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-13 22:14:58 -0400

    initialState for register page and useState

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true
+}
 
 const Register = () => {
+  const [values, setValues] = useState(initialState)
+  
+
   return (
+
     <h1>Register</h1>
   )
 }
