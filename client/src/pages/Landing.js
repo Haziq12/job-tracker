454585649acd46c4801fe659ff93ed1a86b7dd7e
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-13 21:38:52 -0400

    create and import Dashboard, Landing, Register, and Error pages to App.js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from 'react'
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import {Logo} from '../components'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -14,8 +15,8 @@ const Landing = () => {
           <h1>
             Job <span>Tracking</span> App
           </h1>
-          <p>Track your job application process better</p>
-          <button className='btn btn-hero'>Login/Register</button>
+          <p>Track your job application process effectively, gain insights on your progress, and visualize your data</p>
+          <Link to='/register' className='btn btn-hero'>Login/Register</Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
       </div>
