fc5c7f5dcecfc3ba64d6352b5682c7378a89b43d
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-13 21:45:13 -0400

    set up error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 
-export const Error = () => {
-  return (
-    <h1>Error</h1>
-  )
+const Error = () => {
+  return <Wrapper className='full-page'>
+    <div>
+      <img src={img} alt="Not Found" />
+      <h3>Page Not Found</h3>
+      <p>We can't seem to find what you're looking for</p>
+      <Link to='/'>Back Home</Link>
+    </div>
+  </Wrapper>
 }
 
 export default Error
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Register = () => {
+const Register = () => {
   return (
     <h1>Register</h1>
   )
