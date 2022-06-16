094d5a0b6ba27d0775847c97c49e323a3f11a997
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-16 13:32:41 -0400

    set up backend server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Welcome')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server is listening on Port ${port}...`)
+})
