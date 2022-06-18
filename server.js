1abe2826e3ad0bc0c0bda0ceeaf73cbec6ebff53
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-18 16:38:49 -0400

    create databse directory/file and set up mongoose

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+import connectDB from './db/connect.js'
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
@@ -20,3 +22,11 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Server is listening on Port ${port}...`)
 })
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+  } catch (err) {
+      console.log(err)
+  }
+}
