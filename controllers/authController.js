f4ae4780340581e3b95c390c8144eeaad91aa31d
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-19 16:18:14 -0400

    create errors folder, migrate CustomAPIError and BadRequest error to file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,7 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
 
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message)
-  }
-}
 
-class BadRequestError extends CustomAPIError {
-  constructor(message){
-    super(message)
-    this.statusCode = StatusCodes.BAD_REQUEST
-  }
-}
 
 class NotFound extends CustomAPIError {
   constructor(message){
