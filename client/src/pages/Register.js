197702412ddad153158bbac2ff9113f543f071a7
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-21 19:10:33 -0400

    disable button if not member

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -68,7 +68,7 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type='submit' className='btn btn-block'>Submit</button>
+        <button type='submit' className='btn btn-block' disabled={isLoading}>Submit</button>
         <p>
           {values.isMember ? 'Not a member' : 'Already a member'}
           <button type='button' onClick={toggleMember} className='member-btn'>
