d82b0f3e34feef08de3a2b9165e180d834d75efb
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-25 13:38:04 -0400

    destructure user, token, location from data and set up dispatch in registerUser

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -26,14 +26,16 @@ const AppProvider = ({ children }) => {
 
   const clearAlert = () => {
     setTimeout(() => {
-      dispatch({type: CLEAR_ALERT})
+      dispatch({ type: CLEAR_ALERT })
     }, 3000)
   }
 
   const registerUser = async (currentUser) => {
-    dispatch({type: REGISTER_USER_BEGIN})
+    dispatch({ type: REGISTER_USER_BEGIN })
     try {
       const response = await axios.post('/api/v1/auth/register', currentUser)
+      const { user, token, location } = response.data
+      dispatch({ type: REGISTER_USER_SUCCESS, payload: { user, token, location } })
     } catch (err) {
 
     }
