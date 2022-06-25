cdd39da3b599e6bae3cde99eb78b44f94c0fcb3c
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-25 13:40:27 -0400

    if action type is user success in reducer

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,7 +1,7 @@
 import { CLEAR_ALERT, DISPLAY_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR } from "./actions"
 
 const reducer = (state, action) => {
-  if(action.type === DISPLAY_ALERT){
+  if (action.type === DISPLAY_ALERT) {
     return {
       ...state,
       showAlert: true,
@@ -9,7 +9,7 @@ const reducer = (state, action) => {
       alertText: 'Please provide all values'
     }
   }
-  if(action.type === CLEAR_ALERT){
+  if (action.type === CLEAR_ALERT) {
     return {
       ...state,
       showAlert: false,
@@ -18,9 +18,20 @@ const reducer = (state, action) => {
     }
   }
 
-  if(action.type === REGISTER_USER_BEGIN) {
-    return {...state, isLoading: true}
+  if (action.type === REGISTER_USER_BEGIN) {
+    return { ...state, isLoading: true }
   }
+
+  if (action.type === REGISTER_USER_SUCCESS) {
+    return { 
+      ...state, 
+      isLoading: false, 
+      token: action.payload.token, 
+      user: action.payload.user, 
+      userLocation: action.payload.location, 
+      jobLocation: action.payload.location }
+  }
+
   throw new Error(`no such action :${action.type}`)
 }
 export default reducer
