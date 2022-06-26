89d782c19b982dc19432a1c2c6fdeefcf0ad5699
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-26 15:24:31 -0400

    create error if no email or password

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,6 +31,10 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
+  const {email, password} = req.body
+  if(!email || !password) {
+    throw new BadRequestError('Please provide all values')
+  }
   res.send('Login user')
 }
 
