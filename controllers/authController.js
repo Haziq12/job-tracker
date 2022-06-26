cb991ee2b7c4fc6677f445740b05a802e68b2a0f
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-26 15:27:57 -0400

    check if password correct

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 
 
 const register = async (req, res) => {
@@ -34,6 +34,14 @@ const login = async (req, res) => {
   const {email, password} = req.body
   if(!email || !password) {
     throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({email})
+  if(!user) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if(!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials')
   }
   res.send('Login user')
 }
