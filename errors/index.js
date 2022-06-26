a55c93bc5a62c436b13f29b0aff977b733fbed70
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-26 15:20:56 -0400

    import UnAuthenticated to indexJS

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,4 @@
 import BadRequestError from "./bad-request.js";
 import NotFound from "./not-found.js";
-
-export { BadRequestError, NotFound }
+import UnAuthenticatedError from "./unauthenticated.js";
+export { BadRequestError, NotFound, UnAuthenticatedError }
