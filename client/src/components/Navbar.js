4e0c9dbc7ec6547cb7e7444323617b72fb0e0b6d
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-28 14:35:22 -0400

    create nav divs and button with react icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
 import {GrWorkshop} from 'react-icons/gr'
+import {FaAlignLeft, FaUserCircle, FaCaretDown} from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>NavBar</h4>
-      <GrWorkshop/>
+      <div className="nav-center">
+        <button 
+          className='toggle-btn' 
+          onClick={() => console.log('toggle sidebar')}
+          >
+            <FaAlignLeft/>
+        </button>
+      </div>
     </Wrapper>
   )
 }
