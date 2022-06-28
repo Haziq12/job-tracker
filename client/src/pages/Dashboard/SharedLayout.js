e939348cf71d14438ba49a731eff1c5165fd99f8
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-28 14:17:15 -0400

    import comps to SharedLayout, remove nav and insert sidebar

--- client/src/pages/Dashboard/SharedLayout.js
+++ client/src/pages/Dashboard/SharedLayout.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import Wrapper from '../../assets/wrappers/SharedLayout'
-
+import { Navbar, BigSidebar, SmallSidebar } from '../../components'
 
 const SharedLayout = () => {
   return (
     <Wrapper>
-      <nav>
-        <Link to="add-job">Add Job</Link>
-        <Link to="all-jobs">All Job</Link>
-      </nav>
-      <Outlet/>
+      <main className='dashboard'>
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+
+        </div>
+      </main>
+      <Outlet />
     </Wrapper>
   )
 }
