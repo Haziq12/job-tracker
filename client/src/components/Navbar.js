fb10ce045e2b40196bf6248afe2c5c99b831c077
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-28 14:26:26 -0400

    install npm react-icons and test on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import {GrWorkshop} from 'react-icons/gr'
 
 const Navbar = () => {
   return (
     <Wrapper>
       <h4>NavBar</h4>
+      <GrWorkshop/>
     </Wrapper>
   )
 }
