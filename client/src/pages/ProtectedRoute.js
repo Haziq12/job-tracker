22c13e235b06f787b910ac2acb71c6f9a369b6c0
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-28 12:51:32 -0400

    create conditional logic in ProtectedRoute if no user navigate to landing else return children components

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useAppContext } from '../context/appContext'
+import { Navigate } from 'react-router-dom'
+
 
 const ProtectedRoute = ({children}) => {
+  const {user} = useAppContext()
+  if(!user) {
+    return <Navigate to="/landing" /> 
+  }
   return children
 }
 
