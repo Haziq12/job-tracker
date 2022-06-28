91c4ca9fb2d278651a544bc375f8fadf24ac87b3
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-28 12:45:49 -0400

    set up file for protectedRoute and export/imports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,11 @@
 import Register from "./Register";
 import Landing from "./Landing";
 import Error from "./Error";
+import ProtectedRoute from "./ProtectedRoute";
 
 export {
   Landing,
   Register,
-  Error
+  Error,
+  ProtectedRoute
 }
