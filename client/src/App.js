f0c6a2b28ab2acd062bde2ad0a9fa49cd598aa5a
Haziq Naeem <haziqnaeem36@gmail.com>
2022-06-28 12:20:14 -0400

    set up nested routes in appJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Landing, Register, Error } from './pages'
-import {  AddJob,
+import {  
+  AddJob,
   AllJobs,
   Profile,  
   Stats,
-  SharedLayout} from './pages/Dashboard'
+  SharedLayout
+} from './pages/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
   
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/" >
+          <Route path="stats" element={<Stats/>} />
+          <Route path="all-jobs" element={<AllJobs/>} />
+          <Route path="add-job" element={<AddJob/>} />
+          <Route path="profile" element={<Profile/>} />
+        </Route>
         <Route path="/register" element={<Register/>} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error/>} />
