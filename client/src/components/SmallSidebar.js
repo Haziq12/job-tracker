8b98817ee9ed3582499c5565deaf1cc3ae17b7f6
Haziq Naeem <haziqnaeem36@gmail.com>
2022-07-02 15:27:42 -0400

    init SmallSidebar with imports and divs

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
+import Logo from './Logo'
+
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
       <h4>
-        Small Sidebar
+        <div className="sidebar-container show-sidebar">
+          <div className="content">
+            
+          </div>
+        </div>
       </h4>
     </Wrapper>
   )
