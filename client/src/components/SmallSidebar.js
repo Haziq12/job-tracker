c5af404274e38fb6b4bde93782f5d4439e477f36
Haziq Naeem <haziqnaeem36@gmail.com>
2022-07-02 15:31:06 -0400

    small side bar showing

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -13,7 +13,17 @@ const SmallSidebar = () => {
       <h4>
         <div className="sidebar-container show-sidebar">
           <div className="content">
-            
+            <button type='button' className='close-btn' onClick={() => {
+              console.log('toggle sidebar')
+            }}>
+              <FaTimes/>
+            </button>
+            <header>
+              <Logo/>
+            </header>
+            <div className="nav-links">
+              Nav Links 
+             </div>
           </div>
         </div>
       </h4>
