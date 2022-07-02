98a3574ab1586fb5effa6cef8b70ad0de6019138
Haziq Naeem <haziqnaeem36@gmail.com>
2022-07-02 15:25:27 -0400

    complete links objects

--- client/src/utils/links.js
+++ client/src/utils/links.js
@@ -4,5 +4,10 @@ import { FaWpforms } from 'react-icons/fa'
 import { ImProfile } from 'react-icons/im'
 
 const links = [
-  {id: 1, text:'Stats', path:'/', icon:<IoBarChartSharp/>}
+  {id: 1, text:'Stats', path:'/', icon:<IoBarChartSharp/>},
+  {id: 2, text:'All jobs', path:'all-jobs', icon:<MdQueryStats/>},
+  {id: 3, text:'Add Job', path:'add-job', icon:<FaWpforms/>},
+  {id: 4, text:'Profile', path:'profile', icon:<ImProfile/>}
 ]
+
+export default links 
