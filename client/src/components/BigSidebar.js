11d3b09816eb475bac657c85b4d7b8cd3fcfc6ea
Haziq Naeem <haziqnaeem36@gmail.com>
2022-07-04 16:44:36 -0400

    add Navlinks component and Logo to BigSideBar to render links

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -11,9 +11,12 @@ const BigSidebar = () => {
 
   return (
     <Wrapper>
-      <div className={showSidebar?'sidebar-container show-sidebar' : 'sidebar-container'}>
+      <div className={showSidebar?'sidebar-container' : 'sidebar-container show-sidebar'}>
         <div className="content">
-          
+          <header>
+            <Logo/>
+          </header>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
