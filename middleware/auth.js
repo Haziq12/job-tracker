b436788ac0cc881d61b45dc124936d04551fbcc9
Haziq Naeem <haziqnaeem36@gmail.com>
2022-07-21 19:30:17 -0400

    import jwt and set up bearer and token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,12 @@
+import jwt from "jsonwebtoken"
 import { UnAuthenticatedError } from "../errors/index.js"
 
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization
-  if(!authHeader) {
+  if(!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnAuthenticatedError('Authentication Invalid')
   }
+  const token = authHeader.split(' ')[1]
   next()
 }
 
