f2c15eee79a08573d2c1712647282c41275fe5ff
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-04 00:05:44 -0500

    destructure position, company from req.body, throw error if either field is blank, assign useID to req.body.createdBy, and create+save newJob

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,16 @@
+import Job from "../models/Job.js" 
+import { StatusCodes } from "http-status-codes"
+import { BadRequestError, NotFound } from '../errors/index.js'
+
 const createJob = async (req, res) => {
-  res.send('Create Job')
+  const { company, position } = req.body 
+  if(!company || !position) {
+    throw new BadRequestError('Please provide all values') 
+  }
+  req.body.createdBy = req.user.userId 
+  const newJob = await Job.create(req.body) 
+  res.status(StatusCodes.CREATED).json({ newJob })  
+
 }
 const getAllJobs = async (req, res) => {
   res.send('Get All Jobs')
