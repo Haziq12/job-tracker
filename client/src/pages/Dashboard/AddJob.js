9cc1b6eb7b22bdc40c551bcf93e1ea5b2541db3c
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-05 17:09:00 -0500

    destructure values from appContext

--- client/src/pages/Dashboard/AddJob.js
+++ client/src/pages/Dashboard/AddJob.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import { FormRow, Alert } from '../../components/index'
+import { useAppContext } from '../../context/appContext'
+import Wrapper from '../../assets/wrappers/DashboardFormPage'
 
 const AddJob = () => {
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext();
   return (
     <h1>Add Jobs Page</h1>
   )
