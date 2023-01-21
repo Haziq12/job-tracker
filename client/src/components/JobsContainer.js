86581d364a638ab4b9c01df2bced74d637be55cc
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-21 13:35:30 -0500

    create useEffect that will run getJobs function once component mounts

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -1,13 +1,18 @@
 import Wrapper from '../assets/wrappers/JobsContainer'
 import { useAppContext } from '../context/appContext'
 import { useEffect } from 'react'
+import Loading from './Loading'
 import Job from './Job'
 
 const JobsContainer = () => {
   const { getJobs, jobs, isLoading, page, totalJobs } = useAppContext()
+  useEffect(() => {
+    getJobs()
+  }, [])
+
   return (
     <Wrapper>
-      JobsContainer
+      <Loading />
     </Wrapper>
   )
 }
