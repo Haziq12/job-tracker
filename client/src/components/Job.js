f4de63eb24631c858701d6c1e5ab69582d1cbff3
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-23 18:41:33 -0500

    display position and company inside wrapper and div

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -7,13 +7,19 @@ import moment from 'moment'
 
 const Job = ({ company, createdAt, _id, position, jobLocation, jobType, status }) => {
   const { setEditJob, deleteJob } = useAppContext()
+
   let date = moment(createdAt)
   date = date.format('MMM Do, YYYY')
   return (
-    <div>
-      <h5>{company}</h5>
-      <h5>{date}</h5>
-    </div>
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+    </Wrapper>
   )
 }
 
