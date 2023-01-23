1f7494c5ff3e8c1ffb26cdd72346df2d5f55be9a
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-23 18:38:35 -0500

    get relevant imports and props for Job, destructure setEditJOb and deleteJob from useAppContext

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,6 +1,12 @@
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/Job'
+import { link } from 'react-router-dom'
+import JobInfo from './JobInfo'
 import moment from 'moment'
 
-const Job = ({company, createdAt}) => {
+const Job = ({ company, createdAt, _id, position, jobLocation, jobType, status }) => {
+  const { setEditJob, deleteJob } = useAppContext()
   let date = moment(createdAt)
   date = date.format('MMM Do, YYYY')
   return (
