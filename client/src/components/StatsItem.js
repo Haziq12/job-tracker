332239d55bd69b026419c2f44df5bfb355b58923
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-29 15:45:00 -0500

    in StatsItem component, destrcuture props, setup Wrapper, header, span, with appropriate classNames and data from props

--- client/src/components/StatsItem.js
+++ client/src/components/StatsItem.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import Wrapper from '../assets/wrappers/StatItem'
 
-function StatsItem() {
+function StatsItem({title, count, icon, color, bcg}) {
   return (
-    <>
-      <h1>Stats Item</h1>
-    </>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
   )
 }
 
