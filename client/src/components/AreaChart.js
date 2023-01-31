5ce4544529ebba72fdcef0dd3e2d4145d30ec765
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-31 13:48:49 -0500

    create BarChart and AreaChart components

---
+++ client/src/components/AreaChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AreaChart() {
+  return (
+    <h2>Area Chart</h2>
+  )
+}
+
+export default AreaChart
---
+++ client/src/components/BarChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BarChart() {
+  return (
+    <h2>Bar Chart</h2>
+  )
+}
+
+export default BarChart
