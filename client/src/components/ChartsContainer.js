4e34abe0251f96be7143e402b89fd8eb1f0317ad
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-31 14:01:26 -0500

    create button to toggle charts, create onClick function for button that will change barChart state true or false, rendering BarChart component or AreaChart component depending on state avlue

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,13 +1,25 @@
 import Wrapper from "../assets/wrappers/ChartsContainer"
 import { useAppContext } from "../context/appContext"
+import AreaChart from "./AreaChart"
+import BarChart from "./BarChart"
 import { useState } from "react"
 
 function ChartsContainer() {
   const [barChart, setBarChart] = useState(true)
   const {monthlyApplications:data} = useAppContext()
+
+  const toggleBarChart = () => {
+    if(barChart === true) setBarChart(false)
+    else setBarChart(true) 
+  }
   return (
     <Wrapper>
-      
+      <button 
+      className="btn"
+      onClick={toggleBarChart}>
+        Toggle Chart
+      </button>
+      {barChart ? <BarChart data={data}/> : <AreaChart data={data}/>}
     </Wrapper>
   )
 }
