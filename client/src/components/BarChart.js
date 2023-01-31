1ced42219375303c9da0ce2215d15209cf4a715b
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-31 14:26:20 -0500

    start creating BarChart

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -9,7 +9,12 @@ import {
 } from 'recharts';
 
 const BarChartComponent = ({ data }) => {
-
+  <ResponsiveContainer width='100%' height={300}>
+    <BarChart data={data} margin={{top:50}}>
+      <CartesianGrid strokeDasharray='3 3' />
+      <XAxis dataKey={}/>
+    </BarChart>
+  </ResponsiveContainer>
 }
 
 export default BarChartComponent 
