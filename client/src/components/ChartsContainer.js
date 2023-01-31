c1bbe8d8996b5b0bbcdb7a538212232ec8f64d9d
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-31 13:58:04 -0500

    in ChartsContainer, import Wrapper, useAppContext, useState, setState with barChart and setBarChart and destructure monthlyApplications from useAppContext

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import Wrapper from "../assets/wrappers/ChartsContainer"
+import { useAppContext } from "../context/appContext"
+import { useState } from "react"
 
 function ChartsContainer() {
+  const [barChart, setBarChart] = useState(true)
+  const {monthlyApplications:data} = useAppContext()
   return (
-    <div>ChartsContainer</div>
+    <Wrapper>
+      
+    </Wrapper>
   )
 }
 
