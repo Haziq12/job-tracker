97c3729383eea640ef0dbee3bde8c84a20ec11f7
Haziq Naeem <haziqnaeem36@gmail.com>
2023-01-31 14:28:51 -0500

    insert all components for bar chart with values nad props

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -12,7 +12,10 @@ const BarChartComponent = ({ data }) => {
   <ResponsiveContainer width='100%' height={300}>
     <BarChart data={data} margin={{top:50}}>
       <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey={}/>
+      <XAxis dataKey='date' />
+      <YAxis allowDecimals={false} />
+      <Tooltip />
+      <Bar dataKey='count' fill='#2cb1bc' barSize={75}/>
     </BarChart>
   </ResponsiveContainer>
 }
