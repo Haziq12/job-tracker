418d6e47faf5b2ede9ba2cf0366ea30aeb3a9ab9
Haziq Naeem <haziqnaeem36@gmail.com>
2023-02-25 18:33:08 -0500

    in SearchContainer, import required imports and grab state values with useAppCOntext

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import { FormRow, FormRowSelect } from '.'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/SearchContainer'
 
 const SearchContainer = () => {
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters
+  } = useAppContext()
+
   return (
     <h1>
       SearchContainer
