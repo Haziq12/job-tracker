42f16c58a47c12c85b2f9d871ffacb214d08825f
Haziq Naeem <haziqnaeem36@gmail.com>
2023-02-27 17:13:46 -0500

    create totalJobs, numOfPages variables using countDocuments mongoose method, pass to front end

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -25,13 +25,13 @@ const getAllJobs = async (req, res) => {
     createdBy: req.user.userId
   }
 
-    if (status && status !== 'All') {
-      queryObject.status = status
-    }
+  if (status && status !== 'All') {
+    queryObject.status = status
+  }
 
-    if (jobType && jobType !== 'All') {
-      queryObject.jobType = jobType
-    }
+  if (jobType && jobType !== 'All') {
+    queryObject.jobType = jobType
+  }
 
   if (search) {
     queryObject.position = { $regex: search, $options: 'i' }
@@ -39,31 +39,38 @@ const getAllJobs = async (req, res) => {
 
   let result = Job.find(queryObject)
 
-  if(sort === 'Latest') {
+  if (sort === 'Latest') {
     result = result.sort('-createdAt')
   }
 
-  if(sort === 'Oldest') {
+  if (sort === 'Oldest') {
     result = result.sort('createdAt')
   }
 
-  if(sort === 'a-z') {
+  if (sort === 'a-z') {
     result = result.sort('position')
   }
 
-  if(sort === 'z-a') {
+  if (sort === 'z-a') {
     result = result.sort('-position')
   }
 
   const page = Number(req.query.page) || 1
   const limit = Number(req.query.limit) || 10
   const skip = (page - 1) * limit
-  
+
   result = result.skip(skip).limit(limit)
 
   const jobs = await result
 
-  res.status(StatusCodes.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs,
+    numOfPages: numOfPages
+  })
 }
 
 const updateJob = async (req, res) => {
