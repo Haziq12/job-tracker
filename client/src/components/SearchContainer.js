4f8a48a52b4587d1dcbf3f226c680fe77be7aada
Haziq Naeem <haziqnaeem36@gmail.com>
2023-02-27 13:35:44 -0500

    create button that is disable if isLoading is true

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -17,8 +17,8 @@ const SearchContainer = () => {
   } = useAppContext()
 
   const handleSearch = (e) => {
-    if(isLoading) return 
-    handleChange({ name: e.target.name, value:e.target.value })
+    if (isLoading) return
+    handleChange({ name: e.target.name, value: e.target.value })
   }
 
   return (
@@ -26,33 +26,35 @@ const SearchContainer = () => {
       <form className='form'>
         <h4>Search Form</h4>
         <div className="form-center">
-          <FormRow 
-            type='text' 
+          <FormRow
+            type='text'
             name='search'
             value={search}
             handleChange={handleSearch}
-            />
+          />
           <FormRowSelect
             labelText='Status'
-            name='searchStatus' 
+            name='searchStatus'
             value={searchStatus}
-            handleChange={handleSearch} 
+            handleChange={handleSearch}
             list={['All', ...statusOptions]}
           />
           <FormRowSelect
             labelText='Job Type'
-            name='searchType' 
+            name='searchType'
             value={searchType}
-            handleChange={handleSearch} 
+            handleChange={handleSearch}
             list={['All', ...jobTypeOptions]}
           />
           <FormRowSelect
             labelText='Sort'
             value={sort}
-            handleChange={handleSearch} 
+            handleChange={handleSearch}
             list={sortOptions}
           />
-          
+          <button className="btn btn-block btn-danger" disabled={isLoading}>
+            Clear Filters
+          </button>
         </div>
       </form>
     </Wrapper>
