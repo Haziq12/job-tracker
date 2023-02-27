5bb886171151c71a34b4c812c2e7199bc61f9f46
Haziq Naeem <haziqnaeem36@gmail.com>
2023-02-27 17:30:57 -0500

    create pagination buttons using map over pages

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -5,6 +5,11 @@ import { useAppContext } from "../context/appContext"
 const PageBtnContainer = () => {
   const { numOfPages, page } = useAppContext()
 
+  const pages = Array.from({length:numOfPages}, (_, index) => {
+    return index + 1 
+  })
+  console.log(pages)
+
   const nextPage = () => {
     console.log('next page')
   }
@@ -17,10 +22,21 @@ const PageBtnContainer = () => {
     <Wrapper>
       <button className="prev-btn" onClick={prevPage}>
         <HiChevronDoubleLeft />
+        Prev
       </button>
-      <div className="btn-container">Buttons</div>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return <button type='button' className={pageNumber === page ? 'pageBtn active' : 'pageBtn'} 
+          key={pageNumber}
+          onClick={() => console.log('click')}
+          >
+            {pageNumber}
+          </button>
+        })}
+      </div>
       <button className="next-btn" onClick={nextPage}>
         <HiChevronDoubleRight />
+        Next 
       </button>
     </Wrapper>
   )
