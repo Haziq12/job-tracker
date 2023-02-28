290895aa6fe80acac0894490bd2e545ab0090f0c
Haziq Naeem <haziqnaeem36@gmail.com>
2023-02-28 13:01:27 -0500

    after api routes create catch all route that will send index.html file

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.use(express.json())
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
